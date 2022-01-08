7a0d8abd450c2b58c38b6d38d4eb4685697c6efc
Anderson Hsieh <andy13579246810@gmail.com>
2022-01-08 01:40:23 -0500

    add okhttp logging interceptor

--- app/src/main/java/com/AndersonHsieh/wmma_wheremymoneyat/data/network_requests/RetrofitInstance.kt
+++ app/src/main/java/com/AndersonHsieh/wmma_wheremymoneyat/data/network_requests/RetrofitInstance.kt
@@ -1,20 +1,28 @@
 package com.AndersonHsieh.wmma_wheremymoneyat.data.network_requests
 
+import android.net.TrafficStats
 import com.AndersonHsieh.wmma_wheremymoneyat.util.Constants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
+        //the lazy block is only executed on first time use of the variable
 
-    //the lazy block is only executed on first time use of the variable
-    private val retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(Constants.WMMA_API_BASEURI)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
+        private val retrofit by lazy {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
 
-    val apiAccessPoint: TransactionAPI by lazy {
-        retrofit.create(TransactionAPI::class.java);
-    }
+            Retrofit.Builder()
+                .baseUrl(Constants.WMMA_API_BASEURI)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+        val apiAccessPoint: TransactionAPI by lazy{
+            retrofit.create(TransactionAPI::class.java)
+        }
 }
